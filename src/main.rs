4a715fd2ea2a47af9c6bfd82958c4b39083a5e83
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-12-04 19:24:21 -0600

    Start using clap for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 extern crate git2;
 extern crate tempdir;
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 mod repo;
 
 fn main() {
-    let tmp_repo = match repo::create_blank_project(None) {
-        Ok(tmp_repo) => tmp_repo,
-        Err(e) => panic!("failed to init: {}", e),
-    };
-    println!("{:?}", tmp_repo.state());
+    let matches = App::new("mobius-desk")
+        .version("0.1")
+        .args_from_usage
+
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 | _ => println!("Some level of verbosity"),
+    }
+
+    if let Some(init_matches) = matches.subcommand_matches("init") {
+        let filepath = init_matches.value_of("filepath");
+        let new_repo = match repo::create_blank_project(filepath) {
+                Ok(repo) => repo,
+                Err(e) => panic!("failed to init: {}", e),
+        };
+        println!("{:?}", new_repo.state());
+    }
 }
